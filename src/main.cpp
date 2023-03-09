ab456c3d6fd978c7d85b382ac415f46620d64100
Alex <Alex.Schmid91@gmail.com>
2023-03-09 13:03:53 +0100

    Adds Thermocouple readout via MAX31856 Removes unnecessary globals

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
+#include <Wire.h>
 #include <Arduino.h>
 #include <ArduinoRS485.h>
 #include <ArduinoModbus.h>
+#include <Adafruit_MAX31856.h>
 
 // Pin definitions
 const byte pwr_control_pin = 4;
 const byte enable_pin = 2;
+const byte MAX_CS = 10;
 
-// Globals
-int working_output = 0;
-int working_setpoint = 0;
-int tc_readout = 0;
+Adafruit_MAX31856 maxthermo = Adafruit_MAX31856(10);
 
 // Register definitions
 enum Register
@@ -25,6 +25,7 @@ enum Register
   reg_pid_i,
   reg_pid_d,
   reg_software_enable,
+  reg_tc_type,
   reg_tc_error,
   SIZE_REGISTERS
 };
@@ -37,8 +38,6 @@ void setup()
 {
   Serial.begin(9600);
 
-  Serial.println("Modbus RTU Server LED");
-
   // start the Modbus RTU server, with (slave) id 1
   if (!ModbusRTUServer.begin(1, 9600))
   {
@@ -46,13 +45,31 @@ void setup()
     while (1);
   }
 
+  // Initialize to type S thermocouple
+  maxthermo.begin();
+  maxthermo.setThermocoupleType(MAX31856_TCTYPE_S);
+  maxthermo.setConversionMode(MAX31856_CONTINUOUS);
+  maxthermo.setNoiseFilter(MAX31856_NOISE_FILTER_50HZ);
+
   // Everything will be handled by holding registers, all data is stored as 16 bit int (Like Eurotherms do)
   ModbusRTUServer.configureHoldingRegisters(0x00, SIZE_REGISTERS);
 }
 
 void loop()
 {
-  // Read thermocouple and handle errors
+  ModbusRTUServer.poll();
+
+  // Read thermocouple error state and store it in the register, if errors are detected switch the enable register off
+  // If no errors are detected, read temperature
+  ModbusRTUServer.holdingRegisterWrite(reg_tc_error, maxthermo.readFault());
+  if(ModbusRTUServer.holdingRegisterRead(reg_tc_error))
+  {
+    ModbusRTUServer.holdingRegisterWrite(reg_software_enable, 0);
+  }
+  else
+  {
+    ModbusRTUServer.holdingRegisterWrite(reg_working_process_variable, static_cast<int>(maxthermo.readThermocoupleTemperature() * 10 + 0.5));
+  }
 
   if(ModbusRTUServer.holdingRegisterRead(reg_control_mode))
   {
@@ -60,25 +77,16 @@ void loop()
     //working_setpoint_adjust()
     //PID calculation
 
-    set_output_power(working_output);
+    set_output_power(ModbusRTUServer.holdingRegisterRead(reg_working_power));
   }
   else
   {
     set_output_power(ModbusRTUServer.holdingRegisterRead(reg_manual_power));
   }
-  update_working_registers();
 }
 
 void set_output_power(int power)
 {
   // Adjust duty cycle
   // Direct timer manipulation to access 16 birt resolution
-}
-
-void update_working_registers()
-{
-  // Update the registers with the working setpoint and working output
-  ModbusRTUServer.holdingRegisterWrite(reg_working_power, working_output);
-  ModbusRTUServer.holdingRegisterWrite(reg_working_setpoint, working_setpoint);
-  ModbusRTUServer.holdingRegisterWrite(reg_working_process_variable, tc_readout);
 }
