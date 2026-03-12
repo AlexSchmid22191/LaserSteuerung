f706754e3514060a418c731ac4eab23060aa2a00
Alex Schmid <alex.schmid91@gmail.com>
2026-03-12 14:10:13 +0100

    Refactor platformio.ini and remove unused PWM_Test.cpp; enhance safety logic in main control loop; update for hardware version 2 (Revision C PCB); add guards against faulty eeprom reads and invalid user input

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <Wire.h>
 #include <EEPROM.h>
 #include <Arduino.h>
+
+// Prevent the ArduinoRS485 library from defining the default pins, since pin 2 (default DE) is used for the software enable
+#define CUSTOM_RS485_DEFAULT_DE_PIN -1
+#define CUSTOM_RS485_DEFAULT_RE_PIN -1
+
 #include <ArduinoRS485.h>
 #include <ArduinoModbus.h>
 #include <Adafruit_MAX31856.h>
@@ -15,7 +20,7 @@ const uint8_t MAX_FAULT = 8;
 const uint8_t pwr_control_pin = 9;
 const uint8_t MAX_CS = 10;
 
-// Used for the monitor phiotodiode readout via transimpedance amplifier e4xpansion board
+// Used for the monitor photo diode readout via transimpedance amplifier expansion board
 const uint8_t photodiode_readout = A1;
 
 Adafruit_MAX31856 maxthermo = Adafruit_MAX31856(MAX_CS);
@@ -50,20 +55,27 @@ enum EEPROMAdress
   ee_pid_d = 16
 };
 
+enum laser_safety_state
+{
+  disarmed,
+  software_armed,
+  fully_armed
+};
+
 void set_output_power(int power);
 void working_setpoint_adjust();
 void setup_timer();
 void write_to_eeprom();
 void read_from_eeprom();
 void pid_calculation(bool reset = false);
-bool safety_check();
-void shutdown_on_error();
+void cut_power();
+uint16_t sanitize(uint16_t value, uint16_t lower_bound, uint16_t upper_bound, uint16_t fallback);
 
 // About units:
 // All temperature like variables (process_variable, setpoint, error, sum_error and diff_error, pid_p) are stored in tenths of a degree C
 // Integration and derivative time are stored in seconds
 // The time interval is stored in milliseconds
-// The output is stored in hundreths of percents
+// The output is stored in hundredths of percents
 
 void setup()
 {
@@ -78,7 +90,6 @@ void setup()
 
   digitalWrite(sw_enable, LOW);
   digitalWrite(aiming_beam_enable, LOW);
-  digitalWrite(pwr_control_pin, LOW);
   
   setup_timer();
 
@@ -103,26 +114,83 @@ void setup()
 
 void loop()
 {
+  static laser_safety_state safety_state = disarmed;
+  static uint32_t last_arming = millis();
+
   ModbusRTUServer.poll();
   write_to_eeprom();
 
   // Switch the aiming pin
   digitalWrite(aiming_beam_enable, ModbusRTUServer.holdingRegisterRead(reg_aiming_beam));
 
-  if (!safety_check())
+  // Read the thermocouple temperature and write it to the holding register as tenths of degrees
+  ModbusRTUServer.holdingRegisterWrite(reg_working_process_variable, static_cast<int>(maxthermo.readThermocoupleTemperature() * 10 + 0.5));
+
+
+  // Safety logic follows a finite state machine with three states: disarmed, software armed and fully armed.
+  // Refer to the state diagram in the documentation for details
+  switch (safety_state)
   {
-    shutdown_on_error();
-    return;
+  case disarmed:
+    if (ModbusRTUServer.holdingRegisterRead(reg_software_enable) == 1)
+    {
+      digitalWrite(sw_enable, HIGH);
+      safety_state = software_armed;
+    }
+    break;
+  case software_armed:
+    // Trip disarm if thermocouple error is detected and the bypass is not set, otherwise continue
+    // Trip disarm if software enable is cleared, otherwise continue
+    // If the hardware enable is enabled, go to fully armed
+    // Don't trip the power cut if the hardware enable is switched off, otherwise the system would constantly try to switch itself off when only the software enable is switched on but the hardware enable is still off, which would lead to an unstable situation
+    ModbusRTUServer.holdingRegisterWrite(reg_tc_error, maxthermo.readFault());
+    if (ModbusRTUServer.holdingRegisterRead(reg_tc_error) && digitalRead(tc_fault_bypass) == HIGH)
+    {
+      cut_power();
+      safety_state = disarmed;
+    }
+    if (ModbusRTUServer.holdingRegisterRead(reg_software_enable) == 0)
+    {
+      cut_power();
+      safety_state = disarmed;
+    }
+    if (digitalRead(hw_enable_readback) == HIGH)
+    {
+      ModbusRTUServer.holdingRegisterWrite(reg_hardware_enable, 1);
+      safety_state = fully_armed;
+      last_arming = millis();
+    }
+    break;
+  case fully_armed:
+    // Trip disarm if thermocouple error is detected and the bypass is not set
+    // Trip disarm if software enable is cleared, otherwise continue
+    // Trip disarm if hardware enable is switched off, otherwise continue
+    ModbusRTUServer.holdingRegisterWrite(reg_tc_error, maxthermo.readFault());
+    if (ModbusRTUServer.holdingRegisterRead(reg_tc_error) && digitalRead(tc_fault_bypass) == HIGH)
+    {
+      cut_power();
+      safety_state = disarmed;
+    }
+    if (ModbusRTUServer.holdingRegisterRead(reg_software_enable) == 0)
+    {
+      cut_power();
+      safety_state = disarmed;
+    }
+    if (digitalRead(hw_enable_readback) == LOW && millis() - last_arming > 500) // Add a delay to prevent immediate disarming if the hw_enable_readback takes a bit to read high
+    {
+      cut_power();
+      safety_state = disarmed;
+    }
+    break;
   }
-  else
+
+  // If the system is fully armed, perform control calculations and set output power 
+  if (safety_state == fully_armed)
   {
-    ModbusRTUServer.holdingRegisterWrite(reg_working_process_variable, static_cast<int>(maxthermo.readThermocoupleTemperature() * 10 + 0.5));
-    //Safety checks passed, now actually switch the software enable pin, perform control calculations and set output power
-    // If the controller is in manual mode, write the value of the manual power register to the working power register, otherwise adjust the working setpoint (ramp) and perform PID calculations
-    // Finally set the PWM output based on the value of the working power register
-    digitalWrite(sw_enable, ModbusRTUServer.holdingRegisterRead(reg_software_enable));
     if(ModbusRTUServer.holdingRegisterRead(reg_control_mode))
     {
+      // Reset the PID calculation to prevent integral windup while in automatic mode
+      pid_calculation(true);
       ModbusRTUServer.holdingRegisterWrite(reg_working_power, ModbusRTUServer.holdingRegisterRead(reg_manual_power));
     }
     else
@@ -132,47 +200,31 @@ void loop()
     }
     set_output_power(ModbusRTUServer.holdingRegisterRead(reg_working_power)); 
   }
+  else
+  {
+    set_output_power(0);
+  }
 }
 
-bool safety_check()
+void cut_power()
 {
-  // Read thermocouple error state and store it in the register
-  // If errors are detected and the bypass pin is high (jumper not set) return false
-  // If there are no errors of the bypass is set continue
-  ModbusRTUServer.holdingRegisterWrite(reg_tc_error, maxthermo.readFault());
-  if (ModbusRTUServer.holdingRegisterRead(reg_tc_error) && digitalRead(tc_fault_bypass) == HIGH)
-  {
-    return false;
-  }
-  // Check if the hardware enable is set, i.e. if the user has armed the laser via the pushbutton on the device
-  if (digitalRead(hw_enable_readback) == LOW)
-  {
-    return false;
-  }
-  // Fianlly, check if the software enable is set
-  if (ModbusRTUServer.holdingRegisterRead(reg_software_enable) == 0)
-  {
-    return false;
-  }
-  // If all safety checks are passed, return true to allow the controller to operate
-  return true;
-}
-
-void shutdown_on_error()
-{
-  // Switch off the software enable pin, switch to manual mode, set output power to 0 and reset the PID controller
-  digitalWrite(sw_enable, LOW);
+  // If any of the safety checks fail, cut the power to the laser and reset the controller
+  // Switch to manual mode, set output power to 0 and reset the PID controller
   set_output_power(0);
   ModbusRTUServer.holdingRegisterWrite(reg_control_mode, 1);
   ModbusRTUServer.holdingRegisterWrite(reg_working_power, 0);
   ModbusRTUServer.holdingRegisterWrite(reg_manual_power, 0);
+  ModbusRTUServer.holdingRegisterWrite(reg_software_enable, 0);
+  ModbusRTUServer.holdingRegisterWrite(reg_hardware_enable, 0);
+  digitalWrite(sw_enable, LOW);
   pid_calculation(true);
 }
 
 void set_output_power(int power)
 {
   // Adjust duty cycle
-  // Direct timer manipulation to access 16 birt resolution
+  // Direct timer manipulation to access 16 bit resolution
+  power = constrain(power, 0, 10000);
   OCR1A = power;
 }
 
@@ -223,13 +275,20 @@ void write_to_eeprom()
   }
 }
 
+uint16_t sanitize(uint16_t value, uint16_t lower_bound, uint16_t upper_bound, uint16_t fallback)
+{
+  if (value < lower_bound || value > upper_bound)
+    value = fallback;
+  return value;
+}
+
 void read_from_eeprom()
 {
   uint16_t temp;
-  ModbusRTUServer.holdingRegisterWrite(reg_pid_p, EEPROM.get(ee_pid_p, temp));
-  ModbusRTUServer.holdingRegisterWrite(reg_pid_i, EEPROM.get(ee_pid_i, temp));
-  ModbusRTUServer.holdingRegisterWrite(reg_pid_d, EEPROM.get(ee_pid_d, temp));
-  ModbusRTUServer.holdingRegisterWrite(reg_rate, EEPROM.get(ee_rate, temp));
+  ModbusRTUServer.holdingRegisterWrite(reg_pid_p, sanitize(EEPROM.get(ee_pid_p, temp), 1, 10000, 750));
+  ModbusRTUServer.holdingRegisterWrite(reg_pid_i, sanitize(EEPROM.get(ee_pid_i, temp), 1, 10000, 100));
+  ModbusRTUServer.holdingRegisterWrite(reg_pid_d, sanitize(EEPROM.get(ee_pid_d, temp), 0, 10000, 0));
+  ModbusRTUServer.holdingRegisterWrite(reg_rate, sanitize(EEPROM.get(ee_rate, temp), 1, 1200, 15));
 }
 
 void pid_calculation(bool reset)
@@ -238,14 +297,8 @@ void pid_calculation(bool reset)
   // All temperature like variables (process_variable, setpoint, error, sum_error and diff_error, pid_p) are stored in tenths of a degree C
   // Integration and derivative time are stored in seconds
   // The time interval is stored in milliseconds, hence the factors 1000
-  // The output is stored in hundreths of percents
+  // The output is stored in hundredths of percents
   // Shamelessly copied from the Arduino PID Library
-
-  // Timing stuff
-  const uint16_t interval = 100;
-  static uint32_t last_time = millis();
-  uint32_t now = millis();
-  unsigned long timeChange = (now - last_time);
 
   // Memory
   static double last_input = ModbusRTUServer.holdingRegisterRead(reg_working_process_variable);
@@ -253,20 +306,26 @@ void pid_calculation(bool reset)
 
   if(reset)
   {
-    // Reset the error accumulation and the last input to prevent integral windup and large derivative kick when switching from manual to automatic mode or when a thermocouple error is detected and the controller is switched off
-    last_input = ModbusRTUServer.holdingRegisterRead(reg_working_process_variable);
+    // Reset the error accumulation to prevent integral windup and large derivative kick when switching from manual to automatic mode or when a thermocouple error is detected and the controller is switched off
     output_sum = 0;
     return;
   }
+
+  // Timing stuff
+  const uint16_t interval = 100;
+  static uint32_t last_time = millis();
+  uint32_t now = millis();
+  unsigned long timeChange = (now - last_time);
 
   // Control loop
   if (timeChange >= interval)
   {
     // Convert from parallel for to standard form
-    // TODO Units
-    double kp = 10000 / ModbusRTUServer.holdingRegisterRead(reg_pid_p);
-    double ki = kp / ModbusRTUServer.holdingRegisterRead(reg_pid_i) * interval / 1000;
-    double kd = kp * ModbusRTUServer.holdingRegisterRead(reg_pid_d) * 1000 / interval;
+    // Guard against division by zero, by setting the k-terms to zero if the user entered the parallel parameters as zero
+    double kp, ki, kd;
+    kp = ModbusRTUServer.holdingRegisterRead(reg_pid_p) == 0 ? 0 : 10000 / ModbusRTUServer.holdingRegisterRead(reg_pid_p);
+    ki = ModbusRTUServer.holdingRegisterRead(reg_pid_i) == 0 ? 0 : kp / ModbusRTUServer.holdingRegisterRead(reg_pid_i) * interval / 1000;
+    kd = kp * ModbusRTUServer.holdingRegisterRead(reg_pid_d) * 1000 / interval;
 
     /*Compute all the working error variables*/
     double input = ModbusRTUServer.holdingRegisterRead(reg_working_process_variable);
@@ -292,7 +351,8 @@ void working_setpoint_adjust()
 {
   // Adjust the working setpoint by an increment based on the given ramp or until the target SP is reached
   // Only perform the update every 100 milliseconds
-  // The setpoint is written to the Modbus register as 10ths of degrees, but kept internally as [deciDegrees times millisecond per minute] to minimize rounding errors
+  // The setpoint is written to and read from the Modbus register as 10ths of degrees (deci degrees), the ramp in deci degrees per minute
+  // To minimize rounding errors, the increment calculations (increment and last_setpoint) are done in units of [deciDegrees times millisecond per minute]
 
   static uint32_t last_update = millis();
   uint32_t now = millis();
